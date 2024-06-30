6043da84de853ed87e0098e17ed3d969ae6f37ca
Bosco Domingo <boscodomingob@gmail.com>
2024-07-01 00:32:55 +0200

    test: clean up tests

--- test/parallel/test-dotenv-edge-cases.js
+++ test/parallel/test-dotenv-edge-cases.js
@@ -11,23 +11,6 @@ const nodeOptionsEnvFilePath = '../fixtures/dotenv/node-options.env';
 const optionalEnvFilePath = '../fixtures/dotenv/optional.env';
 
 describe('.env supports edge cases', () => {
-
-  it('supports multiple declarations', async () => {
-    // process.env.BASIC is equal to `basic` because the second .env file overrides it.
-    const code = `
-      const assert = require('assert');
-      assert.strictEqual(process.env.BASIC, 'basic');
-      assert.strictEqual(process.env.NODE_NO_WARNINGS, '1');
-    `.trim();
-    const child = await common.spawnPromisified(
-      process.execPath,
-      [ `--env-file=${nodeOptionsEnvFilePath}`, `--env-file=${validEnvFilePath}`, '--eval', code ],
-      { cwd: __dirname },
-    );
-    assert.strictEqual(child.stderr, '');
-    assert.strictEqual(child.code, 0);
-  });
-
   it('supports multiple declarations, including optional ones', async () => {
     const code = `
       const assert = require('assert');
@@ -36,7 +19,7 @@ describe('.env supports edge cases', () => {
     `.trim();
     const child = await common.spawnPromisified(
       process.execPath,
-      [ `--env-file=${nodeOptionsEnvFilePath}`, `--env-file-if-exists=${optionalEnvFilePath}`, "--eval", code ],
+      [ `--env-file=${nodeOptionsEnvFilePath}`, `--env-file-if-exists=${optionalEnvFilePath}`, '--eval', code ],
       { cwd: __dirname },
     );
     assert.strictEqual(child.stderr, '');
@@ -70,11 +53,11 @@ describe('.env supports edge cases', () => {
 
   it('should handle non-existent optional .env file', async () => {
     const code = `
-      require('assert').strictEqual(1, 1)
+      require('assert').strictEqual(process.env.BASIC, undefined);
     `.trim();
     const child = await common.spawnPromisified(
       process.execPath,
-      ["--env-file-if-exists=.env", "--eval", code],
+      ['--env-file-if-exists=.env', '--eval', code],
       { cwd: __dirname },
     );
     assert.strictEqual(child.stderr, '');
